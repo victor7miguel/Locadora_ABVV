c52f24960bd2a26b99484d4cdd941b1eb10d3e17
vinijgc12 <34140281+vinijgc12@users.noreply.github.com>
2022-05-02 14:40:25 -0300

    atualização boilerplate nos controladores

--- src/main/java/com/example/locadora_abvv/negocios/ControladorPessoa.java
+++ src/main/java/com/example/locadora_abvv/negocios/ControladorPessoa.java
@@ -1,29 +1,32 @@
 package com.example.locadora_abvv.negocios;
 
-import com.example.locadora_abvv.dados.ICrudPessoa;
+import com.example.locadora_abvv.dados.IRepositorio;
+import com.example.locadora_abvv.dados.Repositorio;
+import com.example.locadora_abvv.exceptions.ElementoExisteException;
+import com.example.locadora_abvv.exceptions.ElementoNaoExisteExcepcion;
 import com.example.locadora_abvv.negocios.beans.Pessoa;
 
 public class controladorPessoa {
 
-    private ICrudPessoa pessoa;
+    private IRepositorio<Pessoa> repositorioPessoas;
 
-    public controladorPessoa(ICrudPessoa pessoa) {
-        this.pessoa = pessoa;
+    public controladorPessoa(IRepositorio<Pessoa> repositorioPessoas) {
+        this.repositorioPessoas = new Repositorio<>("pessoas.dat");
     }
 
-    public void cadastrar(Pessoa person){
-        this.pessoa.cadastrar(person);
+    public void cadastrar(Pessoa p) throws ElementoExisteException {
+        this.repositorioPessoas.cadastrar(p);
     }
 
-    public Pessoa buscar (String cpf){
-        return this.pessoa.buscar(cpf);
+    public void listar(){
+        this.repositorioPessoas.listar();
     }
 
-    public void remover (String cpf){
-        this.pessoa.remover(cpf);
+    public void remover(Pessoa p) throws ElementoNaoExisteExcepcion {
+        this.repositorioPessoas.remover(p);
     }
 
-    public Pessoa atualizar(Pessoa person){
-        return this.pessoa.atualizar(person);
+    public void atualizar(Pessoa p) throws ElementoNaoExisteExcepcion {
+        this.repositorioPessoas.atualizar(p);
     }
 }
