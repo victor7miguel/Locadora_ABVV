b81834756aebfe4320e8058b35a0586abe65f78f
vinijgc12 <34140281+vinijgc12@users.noreply.github.com>
2022-05-04 15:12:42 -0300

    fxml de algumas telas

--- src/main/java/com/example/locadora_abvv/HelloApplication.java
+++ src/main/java/com/example/locadora_abvv/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tela-Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tela-Adm.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
